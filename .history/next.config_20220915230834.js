35087298395578852fe946da08792754262a3802
lausamtuong <lausamtuong@gmail.com>
2022-09-15 23:10:37 +0700

    'update'

--- .history/next.config_20220915230834.js
+++ .history/next.config_20220915230834.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript:{
-      ignoreBuildErrors:true
-  },
+  typescript
   reactStrictMode: true,
   swcMinify: true,
   images:{
--- .history/next.config_20220915230842.js
+++ .history/next.config_20220915230842.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript:{
-      ignoreBuildErrors:true
+      ig
   },
   reactStrictMode: true,
   swcMinify: true,
